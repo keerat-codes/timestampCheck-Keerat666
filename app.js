cfdaf72edca693823b2e3310bd57388e18764b97
Gurkeerat Singh <gurkeeratsondhi@gmail.com>
2023-08-05 19:20:59 +0530

    Updated app.js using AutoPilot

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ require('dotenv').config();
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+const test  = require('./controllers/test');
+app.use('/controller', test);
 
 const conn = require('./middlewares/connection');
 console.log("Trying to establish a connection to TiDB...");
